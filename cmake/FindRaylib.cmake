51e2b3caf6b550e05a956e7290aeed6a51882dd8
Chouteau Perry <perry.chouteau@epitech.eu>
2025-10-02 10:50:02 +0200

    fix: enable objc for hunterapp

--- cmake/FindRaylib.cmake
+++ cmake/FindRaylib.cmake
@@ -7,6 +7,8 @@ if (NOT ${name}_FOUND) # If there's none, fetch and build raylib
         ${name} #package name
         URL https://github.com/raysan5/raylib/archive/refs/tags/${RAYLIB_VERSION}.tar.gz #where
     )
+    enable_language(OBJC)
+
     #prefer to use FetchContent_MakeAvailable instead of lowerLever customLogic like this FetchContent_GetProperties+FetchContent_Populate
         # Force static library + position independent code
     set(BUILD_SHARED_LIBS OFF CACHE BOOL "Build static libs" FORCE)
